91c0d55d356e946066dcd03590f5d21450f4eaa8
Sydney Bui <sydbui@berkeley.edu>
2020-08-22 17:15:16 -0700

    update site

--- pages/index.js
+++ pages/index.js
@@ -1,76 +1,39 @@
 import Link from 'next/link';
 import Head from '../components/head';
 import Nav from '../components/nav';
-import React, { Component } from 'react';
-// import HomePage from './trivia';
+import React, { useState, Component } from 'react';
 import fetch from 'isomorphic-unfetch';
-import useSWR from '@zeit/swr';
 import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 import {ThemeProvider, Heading, Flex, ButtonGroup, Box, Button} from "@chakra-ui/core";
-
-const API_URL = 'https://opentdb.com/api.php?'
-async function fetcher(path) {
-  const res = await fetch(API_URL + path)
-  const json = await res.json()
-  return json
-}
-
-function HomePage(props) {
-  const string = 'amount=1' + props.category + '&type=multiple'
-  const { data, error } = useSWR(string, fetcher)
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
-  return(
-    <Tabs isFitted>
-      <TabList>
-        <Tab>Question</Tab>
-        <Tab>Answer</Tab>
-      </TabList>
-    
-      <TabPanels>
-        <TabPanel>
-          <Heading>{ReactHtmlParser(data.results[0].question)}</Heading>
-        </TabPanel>
-        <TabPanel>
-          <Heading>{ReactHtmlParser(data.results[0].correct_answer)}</Heading>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-  )
-}
 
 export default class reactApp extends Component{
   constructor() {
     super()
     this.state = {
       contents: "open",
-    //   contents: 
-    //   <Flex flexWrap="wrap" justifyContent="center">         
-    //     <Heading size="xl">Pick a Category</Heading>
-    //     <Box width="100%" height="50px"></Box>
-    // <ButtonGroup spacing={10} onClick={() => this.setState({contents:
-    // <Flex flexWrap="wrap">
-    //   <HomePage category={this.state.category}></HomePage>
-    //   <Box width="100%" height="80px"></Box> 
-    //   <Button onClick={() => this.setState({contents: <HomePage category={this.state.category}></HomePage>})}>NEXT</Button>
-    // </Flex>})}>
-    //       <Button onClick={() => this.setState({category:"&category=9"})}> General Knowledge </Button>
-    //       <Button onClick={() => this.setState({category:"&category=14"})}>Entertainment</Button>
-    //       <Button onClick={() => this.setState({category:"&category=17"})}>Science & Nature</Button>
-    //       <Button onClick={() => this.setState({category:"&category=27"})}>Animals</Button>
-    //       <Button onClick={() => this.setState({category:""})}>Mix It Up!</Button>
-    //     </ButtonGroup>
-    //   </Flex> ,
       category: "",
-      question:"",
-      answer:"",
+      index: 0,
+      data: Object,
     }
   }
+
   componentDidMount() {
-    this.setState({
-      question: <HomePage category={this.state.category}></HomePage>,
-    })
+    fetch('https://opentdb.com/api.php?amount=50')
+    .then((response) => response.json())
+    .then(booksList => {
+        this.setState({ data: booksList});
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if ((prevState.category !== this.state.category) || this.state.index == 48) {
+      fetch('https://opentdb.com/api.php?amount=50'+ this.state.category)
+      .then((response) => response.json())
+      .then(booksList => {
+          this.setState({ data: booksList, index: 0});
+      });
+    }
   }
 
   render() {
@@ -99,16 +62,30 @@ export default class reactApp extends Component{
     )
     if (this.state.contents == "cards") return (
       <ThemeProvider>
-        <Flex justifyContent="center" flexWrap="wrap">
-          <Heading size="2xl">IT'S TRIVIA TIME</Heading>
+        <Flex justifyContent="center" flexWrap="wrap" padding="5%">
+          <Heading size="2xl" onClick={()=> this.setState({contents: "open"})}>IT'S TRIVIA TIME</Heading>
           <Box width="100%" height="20px"></Box>
-          <Box width = "95%">
-          {this.state.question}
+          <Box width ="100%" height="350px" >
+            <Tabs isFitted>
+              <TabList>
+                <Tab>Question</Tab>
+                <Tab>Answer</Tab>
+              </TabList>
+              <TabPanels>
+                <TabPanel>
+                  <Heading>{ReactHtmlParser(this.state.data.results[this.state.index].question)}</Heading>
+                </TabPanel>
+                <TabPanel>
+                  <Heading>{ReactHtmlParser(this.state.data.results[this.state.index].correct_answer)}</Heading>
+                </TabPanel>
+              </TabPanels>
+            </Tabs>
           </Box>
-          <Box width="100%" height="15%"></Box>
-          <Button onClick={() => this.setState({
-            question: <HomePage category={this.state.category}></HomePage>
-          })}>Next</Button>
+          <Box width = "100%" height="25px"></Box>
+          <Flex width= "100%" justifyContent="space-between">
+            <Button onClick={()=> this.setState({contents: "open"})}>Pick a New Category!</Button>
+            <Button onClick={()=> this.setState({index: this.state.index + 1})}>Next</Button>
+          </Flex>
         </Flex>
       </ThemeProvider>
     );
